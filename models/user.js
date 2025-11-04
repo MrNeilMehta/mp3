fdcf6adea401429460dbd7d90693ce35b014bb7d
Neil Mehta <neilbm2@illinois.edu>
2025-11-04 14:52:44 -0600

    Removed duplicate email index warning

--- models/user.js
+++ models/user.js
@@ -1,19 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: [true, 'User name is required'] },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        trim: true,
-        lowercase: true,
-        match: [/^\S+@\S+\.\S+$/, 'Invalid email address']
-    },
-    pendingTasks: { type: [String], default: [] },
-    dateCreated: { type: Date, default: Date.now }
-}, { versionKey: false });
-
-UserSchema.index({ email: 1 }, { unique: true });
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  pendingTasks: [{ type: String }],
+  dateCreated: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.model('User', UserSchema);
